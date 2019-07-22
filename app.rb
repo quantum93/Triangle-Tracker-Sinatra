4b9c57e31da0367ea255bbe284a7b21c0a6cb008
Josh Mabry and Tae Lee <tae@>
2019-07-22 14:31:36 -0700

    This is final of this practice.

--- app.rb
+++ app.rb
@@ -7,3 +7,12 @@ also_reload('lib/**/*.rb')
 get ('/') do
   erb(:triangle)
 end
+
+post ('/triangle_answer') do
+side_one = params[:side1].to_i # ruby always takes string as a default. 
+side_two = params[:side2].to_i
+side_three = params[:side3].to_i
+@triangle = Triangle.new(side_one, side_two, side_three)
+
+erb(:triangle_answer)
+end
--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,11 +7,12 @@ class Triangle
   end
 
   def complete()
+    binding.pry
     if ((@sideOne + @sideTwo <= @sideThree) | (@sideTwo + @sideThree <= @sideOne) | (@sideThree + @sideOne <= @sideTwo))
       @message = "This is a not triangle."
     elsif ((@sideOne == @sideTwo) & (@sideOne == @sideThree))
       @message = "This is an equilateral triangle."
-    elsif ((@sideOne === @sideTwo) | (@sideOne === @sideThree) | (@sideTwo === @sideThree))
+    elsif ((@sideOne == @sideTwo) | (@sideOne == @sideThree) | (@sideTwo == @sideThree))
       @message = "This is an isosceles triangle."
     elsif ((@sideOne != @sideTwo) & (@sideOne != @sideThree) & (@sideTwo != @sideThree))
       @message = "This is a Scalene triangle."
